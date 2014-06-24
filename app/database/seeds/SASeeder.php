6aea244640fcbb23cd673212195bc6d72693e136
Le Thanh Tung <lethanhtungbk@gmail.com>
2014-06-24 14:05:53 +0700

    Update SASeeder

--- app/database/seeds/SASeeder.php
+++ app/database/seeds/SASeeder.php
@@ -27,71 +27,71 @@ class SASeeder extends Seeder {
         //field_types
         DB::table(self::add_prefix('field_types'))->insert(
                 array(
-                    array('id' => 1, 'name' => 'Text Field','uic' => 'textfield'),
-                    array('id' => 2, 'name' => 'Text Area','uic' => 'textarea'),
-                    array('id' => 3, 'name' => 'Dropdownbox','uic' => 'dropdown'),
-                    array('id' => 4, 'name' => 'Listbox','uic' => 'list'),
-                    array('id' => 5, 'name' => 'Checkbox','uic' => 'checkbox'),
-                    array('id' => 6, 'name' => 'Radiobox','uic' => 'radio'),
-                    array('id' => 7, 'name' => 'DatePicker','uic' => 'date'),
-                    array('id' => 8, 'name' => 'Image','uic' => 'image'),
+                    array('id' => 1, 'name' => 'Text Field', 'uic' => 'textfield'),
+                    array('id' => 2, 'name' => 'Text Area', 'uic' => 'textarea'),
+                    array('id' => 3, 'name' => 'Dropdownbox', 'uic' => 'dropdown'),
+                    array('id' => 4, 'name' => 'Listbox', 'uic' => 'list'),
+                    array('id' => 5, 'name' => 'Checkbox', 'uic' => 'checkbox'),
+                    array('id' => 6, 'name' => 'Radiobox', 'uic' => 'radio'),
+                    array('id' => 7, 'name' => 'DatePicker', 'uic' => 'date'),
+                    array('id' => 8, 'name' => 'Image', 'uic' => 'image'),
                 )
         );
-        
+
         //fields
-       DB::table(self::add_prefix('fields'))->insert(
+        DB::table(self::add_prefix('fields'))->insert(
                 array(
-                    array('id'=> 1,'name' => 'Country','code' =>'sa_country','field_type_id' => '3'),
-                    array('id'=> 2,'name' => 'State','code' => 'sa_state','field_type_id' => '3'),
-                    array('id'=> 3,'name' => 'Rank','code'=>'sa_rank','field_type_id' => '1'),
-                    array('id'=> 4,'name' => 'Collage Type','code'=>'sa_collage_type','field_type_id' => '3'),
-                    array('id'=> 5,'name' => 'Programe Type','code'=>'sa_program_type','field_type_id' => '3'),
-                    array('id'=> 6,'name' => 'Discipline','code'=>'sa_discipline','field_type_id' => '3'),
-                    array('id'=> 7,'name' => 'Size','code'=>'sa_size','field_type_id' => '3'),
-                    array('id'=> 8,'name' => 'Collage House Type','code'=>'sa_collage_house_type','field_type_id' => '3'),
+                    array('id' => 1, 'name' => 'Country', 'code' => 'sa_country', 'field_type_id' => '3'),
+                    array('id' => 2, 'name' => 'State', 'code' => 'sa_state', 'field_type_id' => '3'),
+                    array('id' => 3, 'name' => 'Rank', 'code' => 'sa_rank', 'field_type_id' => '1'),
+                    array('id' => 4, 'name' => 'Collage Type', 'code' => 'sa_collage_type', 'field_type_id' => '3'),
+                    array('id' => 5, 'name' => 'Programe Type', 'code' => 'sa_program_type', 'field_type_id' => '3'),
+                    array('id' => 6, 'name' => 'Discipline', 'code' => 'sa_discipline', 'field_type_id' => '3'),
+                    array('id' => 7, 'name' => 'Size', 'code' => 'sa_size', 'field_type_id' => '3'),
+                    array('id' => 8, 'name' => 'Collage House Type', 'code' => 'sa_collage_house_type', 'field_type_id' => '3'),
                 )
         );
-       
-       //field_value
-       DB::table(self::add_prefix('field_values'))->insert(
+
+        //field_value
+        DB::table(self::add_prefix('field_values'))->insert(
                 array(
-                    array('field_id' => 1,'value' => 'United State'),
-                    array('field_id' => 1,'value' => 'United Kindom'),
-                    array('field_id' => 1,'value' => 'France'),
-                    array('field_id' => 1,'value' => 'Germany'),
-                    array('field_id' => 2,'value' => 'California'),
-                    array('field_id' => 2,'value' => 'Corondo'),
-                    array('field_id' => 2,'value' => 'Columbia'),
-                    array('field_id' => 3,'value' => ''),
-                    array('field_id' => 4,'value' => 'Public'),
-                    array('field_id' => 4,'value' => 'Private'),
-                    array('field_id' => 5,'value' => 'ETS'),
-                    array('field_id' => 5,'value' => 'University'),
-                    array('field_id' => 5,'value' => 'Collage'),
-                    array('field_id' => 5,'value' => 'Collage'),
-                    array('field_id' => 6,'value' => 'IT'),
-                    array('field_id' => 6,'value' => 'Math'),
-                    array('field_id' => 6,'value' => 'MBA'),
-                    array('field_id' => 7,'value' => '<500'),
-                    array('field_id' => 7,'value' => '500 - 3000'),
-                    array('field_id' => 7,'value' => '3000 - 10000'),
-                    array('field_id' => 7,'value' => '>10000'),
-                    array('field_id' => 8,'value' => 'Dorm'),
-                    array('field_id' => 8,'value' => 'Family House'),
-                    array('field_id' => 8,'value' => 'No House'),      
+                    array('field_id' => 1, 'value' => 'United State'),
+                    array('field_id' => 1, 'value' => 'United Kindom'),
+                    array('field_id' => 1, 'value' => 'France'),
+                    array('field_id' => 1, 'value' => 'Germany'),
+                    array('field_id' => 2, 'value' => 'California'),
+                    array('field_id' => 2, 'value' => 'Corondo'),
+                    array('field_id' => 2, 'value' => 'Columbia'),
+                    array('field_id' => 3, 'value' => ''),
+                    array('field_id' => 4, 'value' => 'Public'),
+                    array('field_id' => 4, 'value' => 'Private'),
+                    array('field_id' => 5, 'value' => 'ETS'),
+                    array('field_id' => 5, 'value' => 'University'),
+                    array('field_id' => 5, 'value' => 'Collage'),
+                    array('field_id' => 5, 'value' => 'Collage'),
+                    array('field_id' => 6, 'value' => 'IT'),
+                    array('field_id' => 6, 'value' => 'Math'),
+                    array('field_id' => 6, 'value' => 'MBA'),
+                    array('field_id' => 7, 'value' => '<500'),
+                    array('field_id' => 7, 'value' => '500 - 3000'),
+                    array('field_id' => 7, 'value' => '3000 - 10000'),
+                    array('field_id' => 7, 'value' => '>10000'),
+                    array('field_id' => 8, 'value' => 'Dorm'),
+                    array('field_id' => 8, 'value' => 'Family House'),
+                    array('field_id' => 8, 'value' => 'No House'),
                 )
         );
-       
-       //group
-       DB::table(self::add_prefix('groups'))->insert(
+
+        //group
+        DB::table(self::add_prefix('groups'))->insert(
                 array(
                     array('id' => 1, 'name' => 'Collage Properties'),
                 )
         );
-       
-       
-       //group_fields
-       DB::table(self::add_prefix('group_fields'))->insert(
+
+
+        //group_fields
+        DB::table(self::add_prefix('group_fields'))->insert(
                 array(
                     array('group_id' => 1, 'field_id' => '1'),
                     array('group_id' => 1, 'field_id' => '2'),
@@ -103,7 +103,31 @@ class SASeeder extends Seeder {
                     array('group_id' => 1, 'field_id' => '8'),
                 )
         );
-       
+
+        //objects
+        DB::table(self::add_prefix('objects'))->insert(
+                array(
+                    array('group_id' => 1, 'name' => 'Havard University'),
+                    array('group_id' => 1, 'name' => 'Oxford University'),
+                    array('group_id' => 1, 'name' => 'Columbia University'),
+                )
+        );
+        
+        DB::table(self::add_prefix('object_property_values'))->insert(
+                array(
+                    //Havard
+                    array('object_id' => 1, 'field_id' => '1' , 'value' => '1'),
+                    array('object_id' => 1, 'field_id' => '2' , 'value' => '1'),
+                    array('object_id' => 1, 'field_id' => '3' , 'value' => '200'),
+                    array('object_id' => 1, 'field_id' => '4' , 'value' => '1'),
+                    array('object_id' => 1, 'field_id' => '5' , 'value' => '1'),
+                    array('object_id' => 1, 'field_id' => '6' , 'value' => '1'),
+                    array('object_id' => 1, 'field_id' => '6' , 'value' => '2'),
+                    array('object_id' => 1, 'field_id' => '6' , 'value' => '3'),
+                    array('object_id' => 1, 'field_id' => '7' , 'value' => '3'),
+                    array('object_id' => 1, 'field_id' => '8' , 'value' => '1'),
+                )
+        );
     }
 
 }
