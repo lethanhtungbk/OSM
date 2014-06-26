81c33de0fd26d97d2868a403c468dbd64acaf021
Vinh Nguyen <vinh.nguyenthe@harveynash.vn>
2014-06-26 14:32:18 +0700

    edit and add new object

--- app/views/ui/list-action.blade.php
+++ app/views/ui/list-action.blade.php
@@ -3,7 +3,7 @@
         @if (isset($pageData->data->top_action)) 
             @foreach ($pageData->data->top_action as $an_action)
                 <a id="sample_editable_1_new" class="btn green" href="{{$an_action['url']}}">
-                    {{$an_action['label']}} <i class="fa {{$an_action['class']}"></i>
+                    {{$an_action['label']}} <i class="fa {{$an_action['class']}}"></i>
                 </a>
             @endforeach
         @endif
