639e054b533904d8cb19e95db40a0eec40ff0566
Vinh Nguyen <vinh.nguyenthe@harveynash.vn>
2014-06-26 15:19:24 +0700

    margin right for action top

--- app/views/ui/list-action.blade.php
+++ app/views/ui/list-action.blade.php
@@ -2,7 +2,7 @@
     <div class="btn-group">
         @if (isset($pageData->data->top_action)) 
             @foreach ($pageData->data->top_action as $an_action)
-                <a id="sample_editable_1_new" class="btn green" href="{{$an_action['url']}}">
+                <a class="btn green margin-right-10" href="{{$an_action['url']}}">
                     {{$an_action['label']}} <i class="fa {{$an_action['class']}}"></i>
                 </a>
             @endforeach
